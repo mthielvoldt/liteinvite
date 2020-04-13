2b21ce435db54f0e15c5b019dd610d4451e498c7
rileyt <mthielvoldt@gmail.com>
2020-04-13 14:27:46 -0700

    Add email backend to contact form submit

--- app.js
+++ app.js
@@ -278,8 +278,7 @@ app.get('/events/:meetId/invites', (req, res) => {
                     }
                 }
 
-
-                transporter.sendMail(message, (err, info) => {
+                liveTransporter.sendMail(message, (err, info) => {
                     if (err) {
                         console.log('Error occurred. ' + err.message);
                         return process.exit(1);
@@ -287,7 +286,7 @@ app.get('/events/:meetId/invites', (req, res) => {
 
                     console.log('Message sent: %s', info.messageId);
                     // Preview only available when sending through an Ethereal account
-                    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+                    //console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
                 });
             }
         });
@@ -480,6 +479,34 @@ app.post('/login', passport.authenticate('local', {
     failureRedirect: '/login', successRedirect: '/'
 }));
 
+app.post('/contact', (req, res) => {
+    logReq(req);
+
+    let message = {
+        from: 'invitations@liteinvite.com',
+        replyTo: req.body.from,
+        to: "mthielvoldt@gmail.com",
+        subject: req.body.subject,
+        text: req.body.message,
+        dsn: {
+            id: req.body.subject,
+            return: 'headers',
+            notify: ['success', 'failure', 'delay'],
+            recipient: 'mthielvoldt@gmail.com'
+        }
+    };
+    liveTransporter.sendMail(message, (err, info) => {
+        if (err) {
+            console.log('Email Error occurred. ' + err.message);
+            return;// process.exit(1);
+        } else {
+            console.log('Message sent: %s', info.messageId);
+            // Preview only available when sending through an Ethereal account
+            //console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+            res.render('contact-submitted', {authenticated: req.isAuthenticated()});
+        }
+    });
+});
 
 ///////////////////// Helper Functions ///////////////////////////////////
 
@@ -545,10 +572,10 @@ function validEmail(str) {
 
 
 
-const transportOpts = {
+const liveTransportOpts = {
     host: "mail.name.com",
-    port: 25,
-    secure: false,
+    port: 465,
+    secure: true,
     auth: {
         user: "invitations@liteinvite.com",
         pass: process.env.EMAIL_PASS
@@ -563,9 +590,8 @@ const testTransportOpts = {
         pass: 'xhgmFb3HMfRM3mpw1Q'
     }
 }
-
-console.log("transport opts: ", testTransportOpts);
-const transporter = nodemailer.createTransport(testTransportOpts);
+const liveTransporter = nodemailer.createTransport(liveTransportOpts)
+const testTransporter = nodemailer.createTransport(testTransportOpts);
 
 
 
