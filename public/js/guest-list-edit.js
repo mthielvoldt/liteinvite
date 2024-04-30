5cee3490c80116bc23400d55352665748bb04eea
mthielvoldt <mthielvoldt@gmail.com>
2024-04-30 18:08:48 -0400

    Get copy-url working and glowed up

--- public/js/guest-list-edit.js
+++ public/js/guest-list-edit.js
@@ -85,6 +85,7 @@ function GuestHeader(props) {
 
 function GuestLine(props) {
     // Informational line for guests that have already been added.
+    const [tooltip, setTooltip] = useState("Copy guest's url to clipboard");
     let guestStatus;
     if (props.guest.sent === 0) { guestStatus = (<span className="text-muted">Not invited</span>) }
     else {
@@ -104,7 +105,13 @@ function GuestLine(props) {
     }
 
     function copyURL(props) {
-        console.log("copy url")
+        const guestURL = "https://liteinvite.com/events/" + meetId + "?guest=" + props.guest._id;
+        console.log("guests url: " + guestURL);
+        navigator.clipboard.writeText(guestURL);
+        setTooltip("URL Copied!")
+    }
+    function clearTooltip() {
+        setTooltip("Copy guest's url to clipboard");
     }
 
     return (
@@ -115,13 +122,13 @@ function GuestLine(props) {
                 src="/images/Orange_x.svg.png"
                 className="guest-delete-button guest-text"
                 onClick={() => { props.onDelete(props); }} />
-            <button onClick={copyURL} className="btn guest-copy-icon">
+            <button onClick={() => copyURL(props)} onMouseOut={() => clearTooltip()} className="btn guest-copy-icon">
                 <svg aria-hidden="true" focusable="false" role="img" viewBox="0 0 16 16" width="16" height="16" fill="white">
                     <path d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 0 1 0 1.5h-1.5a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-1.5a.75.75 0 0 1 1.5 0v1.5A1.75 1.75 0 0 1 9.25 16h-7.5A1.75 1.75 0 0 1 0 14.25Z"></path>
                     <path d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0 1 14.25 11h-7.5A1.75 1.75 0 0 1 5 9.25Zm1.75-.25a.25.25 0 0 0-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 0 0 .25-.25v-7.5a.25.25 0 0 0-.25-.25Z"></path>
                 </svg>
-                <span data-direction="nw" aria-label="Copy url to clipboard" role="tooltip" className="copy-url-popover">
-                    Copy guest's url to clipboard
+                <span id="copy-url-tooltip" data-direction="nw" aria-label="Copy url to clipboard" role="tooltip" className="copy-url-popover">
+                    {tooltip}
                 </span>
             </button>
 
